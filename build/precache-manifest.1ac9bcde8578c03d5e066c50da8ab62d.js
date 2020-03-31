1c4993562152268414ab1dcd6134449065f83a5f
zniphers <zniphers@gmail.com>
2020-03-30 23:21:40 -0400

    actualizando cosas

---
+++ build/precache-manifest.1ac9bcde8578c03d5e066c50da8ab62d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "865a2ed699497257491df8022dda4886",
+    "url": "/React-proyect/index.html"
+  },
+  {
+    "revision": "ca7b6def3aeace543478",
+    "url": "/React-proyect/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5cdbcacbb139e25fb54f",
+    "url": "/React-proyect/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ca7b6def3aeace543478",
+    "url": "/React-proyect/static/js/2.009ea1d7.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/React-proyect/static/js/2.009ea1d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cdbcacbb139e25fb54f",
+    "url": "/React-proyect/static/js/main.522b9aa4.chunk.js"
+  },
+  {
+    "revision": "acb0791b52ce24fc7bb3",
+    "url": "/React-proyect/static/js/runtime-main.af78590c.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.408f8a54e2f50c5b369c5b299ce3a035.js"
+  "/React-proyect/precache-manifest.1ac9bcde8578c03d5e066c50da8ab62d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-proyect/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
